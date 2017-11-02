c9f5c6458f740598dd6c3df3d517a1aeea29b58e
energister <energister@users.noreply.github.com>
2017-11-02 19:14:18 +0300

    URL builder

---
+++ src/test/java/net/UrlTests.java
@@ -0,0 +1,26 @@
+package net;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UrlTests {
+    private enum Status { CREATED, PROCESSED }
+
+    @Test
+    public void buildUrlWithParameters() throws URISyntaxException {
+        // Arrange
+        URIBuilder builder = new URIBuilder("http://example.com/");
+
+        // Act
+        builder.addParameter("number", String.valueOf(5));
+        builder.addParameter("enum", Status.CREATED.toString());
+        builder.addParameter("paramWithSpace", "some text: and commas");
+
+        // Assert
+        assertEquals("http://example.com/?number=5&enum=CREATED&paramWithSpace=some+text%3A+and+commas", builder.toString());
+    }
+}
